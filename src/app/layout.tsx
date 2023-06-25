76c3a35613b79942a4b722cfb8b30b98c328bff5
Glory <adeyemiglr@gmail.com>
2023-06-26 00:16:57 +0100

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,40 @@
-"use client";
+'use client';
 
-import Navbar from "@/components/Navbar";
-import "../styles/global.css";
-import { Inter } from "next/font/google";
-import { usePathname } from "next/navigation";
+import Navbar from '@/components/Navbar';
+import '../styles/global.css';
+import { Inter } from 'next/font/google';
+import { usePathname } from 'next/navigation';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "LockBox app",
-  description: "Generate and save secure passwords",
+	title: 'LockBox app',
+	description: 'Generate and save secure passwords',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const rendernav = pathname === "/register" ? false : true;
-  console.log(pathname);
+	const pathname = usePathname();
+	const rendernav = pathname === '/register' ? false : true;
+	console.log(pathname);
 
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="./default.png" sizes="any" />
-      </head>
-      <body className={inter.className}>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          {rendernav && <Navbar />}
-          {children}
-        </main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<head>
+				<link rel='icon' href='./default.png' sizes='any' />
+			</head>
+			<body className={inter.className}>
+				<div className='main'>
+					<div className='gradient' />
+				</div>
+				<main className='app'>
+					{rendernav && <Navbar />}
+					{children}
+				</main>
+			</body>
+		</html>
+	);
 }
