75c23925393a4299b93aa010a523631534fec64e
joel oluwafisayo <oluwafisayojoel@gmail.com>
2023-06-27 22:11:26 +0100

    auth design completed

---
+++ public/logo/vector/Logo.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <svg
+      data-v-423bf9ae=""
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 482 90"
+      className="iconLeft"
+      height={180}
+      width={180}
+    >
+      <defs data-v-423bf9ae="">
+        <linearGradient
+          data-v-423bf9ae=""
+          gradientTransform="rotate(25)"
+          id="7c999681-ecda-4aa2-9643-3eeafb0e8e06"
+          x1="0%"
+          y1="0%"
+          x2="100%"
+          y2="0%"
+        >
+          <stop
+            data-v-423bf9ae=""
+            offset="0%"
+            // style={"stop-color: rgb(141, 44, 227); stop-opacity: 1;"}
+          ></stop>
+          <stop
+            data-v-423bf9ae=""
+            offset="100%"
+            // style={"stop-color: rgb(247, 198, 246); stop-opacity: 1;"}
+          ></stop>
+        </linearGradient>
+      </defs>
+      <g
+        data-v-423bf9ae=""
+        id="12c18e11-e488-4070-9d5a-9f6725dfba76"
+        transform="matrix(2.8125,0,0,2.8125,0,0)"
+        stroke="none"
+        fill="url(#7c999681-ecda-4aa2-9643-3eeafb0e8e06)"
+      >
+        <path d="M9.382 8.675h13.943v13.943L32 31.293V0H.707zM22.618 23.325H8.675V9.382L0 .707V32h31.293z"></path>
+      </g>
+    </svg>
+  );
+};
---
+++ src/components/Auth.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Logo } from "../../public/logo/vector/Logo";
+import { FcGoogle } from "react-icons/fc";
+
+const Auth = () => {
+  return (
+    <>
+      {/* signup */}
+      <section className="auth-page">
+        <div className="auth-container">
+          <div className="auth-logo">
+            <Logo />
+          </div>
+
+          <div className="auth-header">
+            <p>join our network</p>
+            <p>
+              We'd love to have you! join our 100% remote network of writers &
+              freelancers
+            </p>
+          </div>
+
+          <div className="auth-google">
+            <span>
+              <FcGoogle size={30} />
+            </span>
+            <p>Sign up with Google</p>
+          </div>
+
+          <div className="auth-span">
+            <span></span>
+            <p>OR</p>
+            <span></span>
+          </div>
+
+          <div className="auth-credentials">
+            <label htmlFor="Name">Name*</label>
+            <input type="text" placeholder="Enter your name" />
+            <label htmlFor="">Email*</label>
+            <input type="text" placeholder="Enter your email" />
+            <label htmlFor="">Password*</label>
+            <input type="password" placeholder="Create a password" />
+          </div>
+
+          <div className="auth-btn">
+            <div className="auth-create type1">
+              <p>Sign up</p>{" "}
+            </div>
+
+            <div className="auth-direct">
+              <p>
+                Already have an account?<span>Log in</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* login */}
+    </>
+  );
+};
+
+export default Auth;
